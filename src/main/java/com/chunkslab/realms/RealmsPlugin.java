a33ca2fbe6e701211a2bd33a72cdfd736c0b27ee
Emre <amowny08@gmail.com>
2024-10-18 23:02:35 +0300

    Initial commit

---
+++ src/main/java/com/chunkslab/realms/RealmsPlugin.java
@@ -0,0 +1,17 @@
+package com.chunkslab.realms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RealmsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
